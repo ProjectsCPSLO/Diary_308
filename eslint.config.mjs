b80ca97be4491c299ad6db1cdc15afda623c5477
Amarbir Janda <129787119+AmarbirJanda@users.noreply.github.com>
2025-02-25 15:35:02 -0800

    Changing eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,46 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
+  js.configs.recommended,
+
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
-      globals: globals.browser,
-    },
-    plugins: {
-      js: pluginJs.configs.recommended,
-      react: pluginReact.configs.recommended,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
-    ],
-    env: {
-      browser: true,
-      node: true,
-      es6: true,
-      jest: true,
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': 'error',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
 ];
