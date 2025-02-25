56a761fd731649dd3254fbe601ce7fe740a932eb
Amarbir Janda <129787119+AmarbirJanda@users.noreply.github.com>
2025-02-25 15:49:58 -0800

    Chaning eslintrc and getting rid of unused imports in post.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,11 +1,22 @@
 module.exports = {
-  env: {
-    node: true,
-    jest: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-};
+    env: {
+      node: true,
+      jest: true,
+      es6: true
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    globals: {
+      describe: 'readonly',
+      it: 'readonly',
+      expect: 'readonly',
+      beforeEach: 'readonly',
+      afterEach: 'readonly',
+      beforeAll: 'readonly',
+      afterAll: 'readonly',
+      jest: 'readonly'
+    }
+  };
