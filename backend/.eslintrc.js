d1e1304576921f27fea4aa4ebeb5534e3a05abcd
Amarbir Janda <129787119+AmarbirJanda@users.noreply.github.com>
2025-02-25 15:44:14 -0800

    Adding eslintrc file

---
+++ backend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      node: true,
+      jest: true
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    }
+  }
