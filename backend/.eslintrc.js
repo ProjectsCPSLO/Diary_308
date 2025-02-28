039bb627ef6d3fc172741dc592ee050b1da4201c
Amarbir Janda <129787119+AmarbirJanda@users.noreply.github.com>
2025-02-27 16:38:57 -0800

    Changing .eslintrc

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,22 +1,8 @@
 module.exports = {
-    env: {
-      node: true,
-      jest: true,
-      es6: true,
-    },
-    extends: ['eslint:recommended'],
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    globals: {
-      describe: 'readonly',
-      it: 'readonly',
-      expect: 'readonly',
-      beforeEach: 'readonly',
-      afterEach: 'readonly',
-      beforeAll: 'readonly',
-      afterAll: 'readonly',
-      jest: 'readonly',
-    },
-  };
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  ignorePatterns: ['backend_tests/**'],
+};
