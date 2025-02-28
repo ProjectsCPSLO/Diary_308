cccec49562a2f3ec7880b654ac89f904906261f0
Amarbir Janda <129787119+AmarbirJanda@users.noreply.github.com>
2025-02-27 16:41:36 -0800

    Fixing eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  env: {
-    node: true,
-    jest: true,
-  },
-  extends: ['eslint:recommended'],
-  ignorePatterns: ['backend_tests/**'],
-};
+    env: {
+      node: true,
+      jest: true,
+      es6: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    ignorePatterns: ['backend_tests/**'],
+  };
