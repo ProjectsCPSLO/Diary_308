6f5d9b40370bcad5aa9beef9217e284c9375c38b
Pranav Nallaperumal <pnallape@calpoly.edu>
2025-03-11 12:49:28 -0700

    changed to azure

--- frontend/src/components/EditPostForm.jsx
+++ frontend/src/components/EditPostForm.jsx
@@ -51,7 +51,7 @@ const EditPostForm = ({ post, open, onClose, theme }) => {
 
     try {
       const response = await fetch(
-        `http://localhost:4000/api/posts/${post._id}/verify`,
+        `https:/diary-backend-d7dxfjbpe8g0cchj.westus3-01.azurewebsites.net//api/posts/${post._id}/verify`,
         {
           method: 'POST',
           headers: {
